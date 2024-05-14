9520fa5cdb5a5f907f6d9089d50811a80783c70f
pckuo <b04201003@gmail.com>
2024-05-14 16:57:06 +0800

    import data finished

--- import_data.py
+++ import_data.py
@@ -1,8 +1,9 @@
-# import_data.py
-import csv
-from datetime import datetime
-
-
+import pandas as pd
+# import talib
+# import numpy as np
+# import matplotlib.pyplot as plt
+import yfinance as yf
+import sys
 import os
 import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'invest_adviser.settings')
@@ -10,28 +11,42 @@ django.setup()
 
 from investment.models import StockData
 
-def import_stock_data(csv_file_path):
-    with open(csv_file_path, 'r') as file:
-        reader = csv.reader(file)
-        next(reader)  # Skip header row
-        for row in reader:
-            ticker = row[0]
-            date = datetime.strptime(row[1], '%Y-%m-%d').date()
-            open_price = float(row[2])
-            high_price = float(row[3])
-            low_price = float(row[4])
-            close_price = float(row[5])
-            volume = int(row[6])
-            stock_data = StockData.objects.create(
-                Ticker=ticker,
-                Date=date,
-                Open=open_price,
-                High=high_price,
-                Low=low_price,
-                Close=close_price,
-                Volume=volume
-            )
-            stock_data.save()
+def get_data(ticker):
+    data = yf.download(ticker, start='2024-04-01', end='2024-05-01')
+    data['Ticker'] = ticker
+    data = data.reset_index()
+    data['Date'] = pd.to_datetime(data['Date'])
+    data['Date'] = data['Date'].dt.strftime('%Y-%m-%d')
+    data = data.reindex(columns=['Ticker', 'Date', 'Open', 'High', 'Low', 'Close', 'Volume'])
+    return data
 
-# 调用导入函数
-import_stock_data('data.csv')
+def import_stock_data(ticker):
+    data = get_data(ticker)
+    for item in data.itertuples():
+        ticker = item.Ticker
+        date = item.Date
+        open_price = float(item.Open)
+        high_price = float(item.High)
+        low_price = float(item.Low)
+        close_price = float(item.Close)
+        volume = int(item.Volume)
+        stock_data = StockData.objects.create(
+            Ticker=ticker,
+            Date=date,
+            Open=open_price,
+            High=high_price,
+            Low=low_price,
+            Close=close_price,
+            Volume=volume
+        )
+        stock_data.save()
+
+
+if __name__ == '__main__':
+    ticker = sys.argv[1]
+    print("Importing data for {}".format(ticker))
+    try:
+        import_stock_data(ticker)
+        print("Data for {} has been imported".format(ticker))
+    except Exception as e:
+        print("Error importing data for {}: {}".format(ticker, e))
